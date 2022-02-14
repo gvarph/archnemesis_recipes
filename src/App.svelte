9407101d0041e15cb87be47058ae4d1a1f557dc5
Krul, Filip <krulfili@fel.cvut.cz>
2022-02-14 06:43:40 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    //  export let name: string;
+
+    import recipes from "./recipes.json";
+    import MR from "./mr.svelte";
+    let monsters = recipes.monsters;
+
+    let default_mobs = [
+        { name: "innocence-touched", hidden: false },
+        { name: "brine king-touched", hidden: false },
+        { name: "kitava-touched", hidden: false },
+        { name: "evocationist", hidden: false },
+        { name: "shakari-touched", hidden: false },
+        { name: "arakali-touched", hidden: false },
+    ];
+</script>
+
+<main>
+    <flexbox>
+        {#each default_mobs as s}
+            <button
+                class="hide_button"
+                style="opacity: {s.hidden ? '0.5' : '1'};"
+                on:click={() => (s.hidden = !s.hidden)}>{s.name}</button
+            >
+        {/each}
+    </flexbox>
+    <div>
+        {#each default_mobs as s}
+            {#if !s.hidden}
+                <MR monster_name={s.name} />
+            {/if}
+        {/each}
+    </div>
+</main>
+
+<style>
+    .hide_button {
+        margin: 2.5px;
+        padding: 5px;
+    }
+</style>
---
+++ src/mr.svelte
@@ -0,0 +1,67 @@
+<script>
+    let is_complete = false;
+    export let parent_complete = false;
+    export let monster_name;
+    import recipes from "./recipes.json";
+    let children = [];
+    let collapsed = false;
+
+    //get monster with name equal to monster_name
+    let monster = recipes.monsters.find((monster) => monster.name === monster_name);
+
+    console.log(monster);
+    if (monster !== undefined) {
+        if (monster.children !== undefined) {
+            children = monster.children;
+        }
+    }
+</script>
+
+<div class="main {is_complete && !parent_complete ? 'complete' : 'not_complete'}">
+    <div class="info">
+        <div>
+            <input type="checkbox" id="checkbox" bind:checked={is_complete} />
+            {monster_name}
+        </div>
+    </div>
+    {#if children.length > 0 && !collapsed}
+        <div class="sub_recipe">
+            {#each children as child}
+                <svelte:self monster_name={child} parent_complete={is_complete} />
+            {/each}
+        </div>
+    {/if}
+</div>
+
+<style>
+    .main {
+        /* border opacity 50% */
+        border: 1px solid rgba(0, 0, 0, 0.5);
+        border-radius: 10px;
+
+        padding: 5px;
+        margin: 2.5px;
+        display: flex;
+    }
+    .complete {
+        opacity: 0.5;
+    }
+    .not_complete {
+        opacity: 1;
+    }
+    .info {
+        float: left;
+        /* center vertically */
+        display: flex;
+        align-items: center;
+    }
+    .collapsed_c {
+        transform: rotate(90deg);
+    }
+    .open_c {
+        transform: rotate(0deg);
+    }
+    .collapsible {
+        background: transparent url("https://img.icons8.com/ios-glyphs/30/000000/expand--v1.png");
+    }
+</style>
